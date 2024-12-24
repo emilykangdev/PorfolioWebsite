8bbf1beb65ea138286ecced14b9a8464617f9efa
Emily Kang <emilykang@Emilys-MacBook-Pro-3.local>
2024-12-24 00:44:26 -0800

    add  https to base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/emily-kang77.github.io/',
+  base: "https://emily-kang77.github.io/",
 })
