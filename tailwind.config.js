bd1e03b90d82ebde952b9d04435bc38a82418767
Emily-Kang77 <emilykang135@gmail.com>
2024-12-29 11:41:40 -0800

    remove require() since it's not defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 import daisyui from 'daisyui'
-const colors = require('tailwindcss/colors')
+// const colors = require('tailwindcss/colors')
 
 const colorNames = {
   
@@ -15,14 +15,14 @@ export default {
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
+      // transparent: 'transparent',
+      // current: 'currentColor',
+      // black: colors.black,
+      // white: colors.white,
+      // gray: colors.gray,
+      // emerald: colors.emerald,
+      // indigo: colors.indigo,
+      // yellow: colors.yellow,
       "techcolor": "#84cc16",
       // primary: '#f8875c',
       // secondary: '#7CAD9D',
