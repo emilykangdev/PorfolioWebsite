e56a08959c32771a72f34c191c0c7a1e72c3f611
Emily-Kang77 <emilykang135@gmail.com>
2025-07-19 17:16:59 -0700

    update linkedin link

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -5,7 +5,7 @@ const ContactInfo = () => {
     return (
         <div className="icon-group flex mt-5 justify-center mx-auto">
 
-                <a href={"https://www.linkedin.com/in/emilykang1/"} target="_blank" rel="noopener noreferrer" className="mr-5">
+                <a href={"https://www.linkedin.com/in/emilykangdev/"} target="_blank" rel="noopener noreferrer" className="mr-5">
                                         <FaLinkedin size={45} color="white"/>
                                     </a>
 
